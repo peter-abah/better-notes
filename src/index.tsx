39102fae62b4a34dc2d43c2e1b3cb80b4bd7b45e
Peter Abah <peterabah.ob@gmail.com>
2022-08-24 16:12:10 +0100

    Add service workers

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import "./styles/index.css";
 import App from "./App";
@@ -21,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
